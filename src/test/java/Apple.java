2718565cc1fd51b946bcee5357f2198eaccd5612
Vrushal123456 <161623577+Vrushal123456@users.noreply.github.com>
2024-04-23 14:55:00 +0530

    vrushal commit

---
+++ src/test/java/Apple.java
@@ -0,0 +1,22 @@
+
+public class Apple {
+
+	public static void main(String[] args) {
+		add();
+
+	}
+public static void add() {
+	
+	int a=25;
+	int b=65;
+	
+int c ;
+
+c=a+b;
+
+System.out.println("additon will"+c);
+
+
+	
+}
+}
---
+++ src/test/java/Peru.java
@@ -0,0 +1,23 @@
+
+public class Peru {
+	
+	
+	public static void method() {
+		
+		int b;
+		int c;
+		int d;
+		 
+		System.out.print("this is class is peru");
+		
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		System.out.print("this is class is peru");
+		
+	}
+
+}
